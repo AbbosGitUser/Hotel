42edf17ae6597525e5fbd028a12670d0a27bd9fc
shahobiddin2005 <uralovshahobiddin2005@gmail.com>
2024-05-14 16:40:27 +0500

    Shahobiddin added some service 2

--- GitBranchShaxa.java
+++ GitBranchShaxa.java
@@ -4,4 +4,10 @@ public class GitBranchShaxa {
     public void firstMethod(){
         System.out.println("first changed!");
     }
+    public void firstMethod1(){
+        System.out.println("first changed1!");
+    }
+    public void firstMethod2(){
+        System.out.println("first changed2!");
+    }
 }
